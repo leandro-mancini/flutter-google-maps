e93aa50661a7a4e2a7076ccedd7e9852b7f71772
Leandro Mancini de Sousa <leandro.sousa@bgmrodotec.com.br>
2019-05-21 17:19:45 -0300

    Floating Action Button Maps

--- lib/presentation/pages/postos.page.dart
+++ lib/presentation/pages/postos.page.dart
@@ -13,7 +13,7 @@ class _PostosPageState extends State<PostosPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Google Maps'),
+        title: Text('Abastecimento'),
       ),
       body: Stack(
         children: <Widget>[
@@ -30,31 +30,29 @@ class _PostosPageState extends State<PostosPage> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: EdgeInsets.all(20),
             child: Align(
               alignment: Alignment.bottomRight,
               child: FloatingActionButton(
-                onPressed: mapController == null
-                    ? null
-                    : () {
-                  mapController
-                      .animateCamera(CameraUpdate.newCameraPosition(
-                    const CameraPosition(
-                      target: LatLng(-23.6365, -46.6452),
-                      tilt: 30,
-                      zoom: 18,
-                    ),
-                  ));
-                },
-                mini: true,
+                backgroundColor: Colors.white,
                 child: Icon(
                   Icons.my_location,
                   color: Colors.black54,
                 ),
-                backgroundColor: Colors.white,
+                onPressed: () => {
+                  mapController.animateCamera(
+                    CameraUpdate.newCameraPosition(
+                      CameraPosition(
+                        target: LatLng(-23.6365, -46.6452),
+                        tilt: 30,
+                        zoom: 18,
+                      )
+                    ),
+                  ),
+                },
               ),
             ),
-          ),
+          )
         ],
       ),
     );
