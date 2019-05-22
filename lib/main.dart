7c9e5bfb059de6389ef1e5200d55a0f0c7a5c960
Leandro Mancini de Sousa <leandro.sousa@bgmrodotec.com.br>
2019-05-22 01:08:00 -0300

    Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:abastecimento/presentation/pages/login.page.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -11,10 +12,123 @@ class MyApp extends StatelessWidget {
       title: 'Abastecimento',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        primaryColor: Colors.red,
+        accentColor: Color(0xFF6200EE),
         primarySwatch: Colors.deepPurple,
         canvasColor: Colors.transparent
       ),
-      home: LoginPage(),
+      // home: LoginPage(),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () => {
+      Navigator.push(
+        context, 
+        MaterialPageRoute(
+          builder: (context) => LoginPage(),
+        )
+      )
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        // radius: 50,
+                        // child: Icon(
+                        //   Icons.shopping_cart,
+                        //   color: Colors.greenAccent,
+                        //   size: 50,
+                        // ),
+                        child: SizedBox(
+                          width: 60,
+                          height: 60,
+                          child: Image.asset('assets/ic_launcher.png'),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: 20,
+                        ),
+                      ),
+                      Text(
+                        'Abastecimento',
+                        style: TextStyle(
+                          color: Color(0xFF6200EE),
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        'Sua viagem em ordem',
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: 20,
+                      ),
+                    ),
+                    Text(
+                      'Carregando aguarde...',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
